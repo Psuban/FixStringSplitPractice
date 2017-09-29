9984ac6a9c647764a1b2f6fc5172094bca356d49
Purushothmakumar, Suban (UK) <suban.purushothmakumar@euromoneyplc.com>
2017-09-29 16:52:39 +0100

    Moved first attempt from main method into it's own class

---
+++ StringSplitFIXPractive/StringSplitFIXPractive/Attemp1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplitFIXPractive
+{
+    class Attemp1
+    {
+        public void StringDelimeterExercise1()
+        {
+
+            string[] delimiterCharSpace = { " ", "=" };
+            string test =
+                "8=FIX.4.4 9=0000324 35=W 49=SENDER 56=RECEIVER 34=18180 52=20170530-01:05:02.907 1369=164 262=15 55=CA";
+            Console.WriteLine(test);
+            string[] fixsohdelimited;
+            fixsohdelimited = test.Split(delimiterCharSpace, StringSplitOptions.None);
+            foreach (string s in fixsohdelimited)
+            {
+                Console.WriteLine(s);
+            }
+            string[] fixField = new string[fixsohdelimited.Length];
+            string[] fixValue = new string[fixsohdelimited.Length];
+            Console.WriteLine("\nBelow are the Fix Field Tags\n");
+            for (int i = 0; i < fixsohdelimited.Length;)
+            {
+                fixField[i] = fixsohdelimited[i];
+                Console.WriteLine("Fix Field " + i + " is " + fixField[i]);
+                i = i + 2;
+
+            }
+            Console.WriteLine("\nBelow are the Fix Value Tags\n");
+            for (int i = 1; i < fixsohdelimited.Length;)
+            {
+                fixValue[i] = fixsohdelimited[i];
+                Console.WriteLine("Fix Value " + i + " is " + fixValue[i]);
+                i = i + 2;
+            }
+        }
+    }
+}
--- StringSplitFIXPractive/StringSplitFIXPractive/Program.cs
+++ StringSplitFIXPractive/StringSplitFIXPractive/Program.cs
@@ -8,43 +8,18 @@ namespace StringSplitFIXPractive
 {
     class Program
     {
+        private class FixField
+        {
+            public string Tag { get; set; }
+
+            public string Value { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            string[] delimiterCharSpace = { " ","="};
-            string test = "8=FIX.4.4 9=0000324 35=W 49=SENDER 56=RECEIVER 34=18180 52=20170530-01:05:02.907 1369=164 262=15 55=CA";
-            Console.WriteLine(test);
-            string[] fixsohdelimited;
-            fixsohdelimited = test.Split(delimiterCharSpace, StringSplitOptions.None);
-            foreach (string s in fixsohdelimited)
-            {
-                Console.WriteLine(s);
-            }
-
-            string[] fixField = new string[fixsohdelimited.Length];
-            string[] fixValue = new string[fixsohdelimited.Length];
-            Console.WriteLine("\nBelow are the Fix Field Tags\n");
-            for (int i = 0; i < fixsohdelimited.Length;)
-            {
-                
-                fixField[i] = fixsohdelimited[i];
-                Console.WriteLine("Fix Field " + i + " is " + fixField[i]);
-                i = i + 2;
-                
-            }
-            Console.WriteLine("\nBelow are the Fix Value Tags\n");
-            for (int i = 1; i < fixsohdelimited.Length;)
-                {
-                    fixValue[i] = fixsohdelimited[i];
-                    Console.WriteLine("Fix Value " + i + " is " + fixValue[i]);
-                i = i + 2;
-
-                }
-
-
-
-
-
-
+            Attemp1 attempt1 = new Attemp1();
+            attempt1.StringDelimeterExercise1();
+        }
         }
     }
-}
+
