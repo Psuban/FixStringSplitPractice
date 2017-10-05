25e75eb13e3a336950543b582f5e784c5d43fdec
Purushothmakumar, Suban (UK) <suban.purushothmakumar@euromoneyplc.com>
2017-10-05 19:50:05 +0100

    Refined Attempt 2 with the help of Dharsh to use a performing method of splitting the string. Removed if statement and changed the while condition. Also introduced a new global variable, start index , to be used in the while condition and the start index for the index of equals

--- StringSplitFIXPractive/StringSplitFIXPractive/Attempt2.cs
+++ StringSplitFIXPractive/StringSplitFIXPractive/Attempt2.cs
@@ -14,40 +14,29 @@ namespace StringSplitFIXPractive
             string test = "8=FIX.4.4 9=0000324 35=W 49=SENDER 56=RECEIVER 34=18180 52=20170530-01:05:02.907 1369=164 262=15 55=CA ";
             List<string> tagList = new List<string>();
             List<string> valueList = new List<string>();
-            int i = 1;
+            int i = 0;
 
-            while (test.Length!=0)
+          
+
+            int startIndex = 0;
+            
+
+            while (startIndex!=test.Length)
             {
-                Console.WriteLine("This is the " + i + " iteration");
                 i++;
-                int indexOfEquals = test.IndexOf('=');
-                Console.WriteLine("This is the index number of the equals sign " + indexOfEquals);
-                tagList.Add(test.Substring(0, indexOfEquals));
-                //string tag = test.Substring(0, indexOfEquals);
-                //Console.WriteLine("The tag value is: " + tag);
-                if (test.Contains(' '))
-                {
-                    int indexOfSpace = test.IndexOf(' ');
-                    Console.WriteLine("This is the index number of the space " + indexOfSpace + "\n");
-                    valueList.Add(test.Substring(indexOfEquals + 1, indexOfSpace - (indexOfEquals + 1)));
-                    //string value = test.Substring(indexOfEquals + 1, indexOfSpace - (indexOfEquals + 1));
-                    //Console.WriteLine(value);
-
-                    test = test.Substring(indexOfSpace + 1, test.Length - (indexOfSpace + 1));
-                }
-                else
-                {
-                    break;
-                }
-                Console.WriteLine(test);
+                int indexOfEquals = test.IndexOf('=', startIndex);
+                tagList.Add(test.Substring(startIndex, indexOfEquals-startIndex));
+                int indexOfSpace = test.IndexOf(' ', indexOfEquals + 1);
+                startIndex = indexOfSpace + 1;
+                valueList.Add(test.Substring(indexOfEquals + 1, indexOfSpace - (indexOfEquals + 1)));
 
             }
-            Console.WriteLine("\nthe number of tags is: " + tagList.Count);
+            Console.WriteLine("\nThe number of tags is: " + tagList.Count);
             foreach (string t in tagList)
             {
                 Console.WriteLine(t);
             }
-            Console.WriteLine("\nthe number of values is: " + valueList.Count);
+            Console.WriteLine("\nThe number of values is: " + valueList.Count);
             foreach (string v in valueList)
             {
                 Console.WriteLine(v);
---
+++ StringSplitFIXPractive/StringSplitFIXPractive/WebRequest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplitFIXPractive
+{
+    class WebRequest1
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.metalbulletin.com/xmlfeed/xml_prices_full.asp?code=99203&user=hoganas&pass=hoga2016");
+        
+    }
+}
